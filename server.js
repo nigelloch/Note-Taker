e853660dc2bd6f37b786a639f6e387f2c8787fd6
loch <nigelloch@gmail.com>
2022-02-04 19:59:02 -0600

    added server, created html routes

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const indexRoutes = require('./routes/indexRoutes');
+const notesRoutes = require('./routes/notesRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', notesRoutes);
+app.use('/', indexRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
